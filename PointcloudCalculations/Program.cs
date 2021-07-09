832558dd3a52a2d84cb02848ab57c88f7976d96a
Hooft, C.A. <hoof1200@student.nhl.nl>
2021-07-09 15:56:07 +0200

    Basic commit, no code yet but there are comments about what the code will have to do

--- PointcloudCalculations/Program.cs
+++ PointcloudCalculations/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace PointcloudCalculations
 {
+    /// <summary>
+    /// Make a cone from the camera position
+    /// Take all points within the where te angle is less than angle tollerance(2-5 degrees)
+    /// Sort all of these points based on the shortest distance to the camera position
+    /// build a selection based on distance based on x,y,z distance to camera positiom
+    /// Than sort selection with the shortest distance at the start of the array/ list/ collection/ other way to safe information'
+    /// 
+    /// 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
